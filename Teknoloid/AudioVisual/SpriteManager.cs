404393b9283f50ccd5701af7f686f812c33d58d3
Roxie Wattz <roxie@flux3on.xyz>
2022-03-30 11:24:21 -0700

    sprite manager base code

---
+++ Teknoloid/AudioVisual/SpriteManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Teknoloid.AudioVisual
+{
+    public static class SpriteManager
+    {
+        public static List<Sprite> SpriteList { get; set; }
+
+        public static int DrawSprites(SpriteBatch sb, GameContent gc)
+        {
+            try
+            {
+                
+                return 0;
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return -1;
+            }
+        }
+
+    }
+}
