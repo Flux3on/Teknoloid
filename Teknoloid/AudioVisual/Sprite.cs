7ac9aa5acd7a67c4c9609361fbbd29871f1db239
Roxie Wattz <roxie@flux3on.xyz>
2022-03-30 10:49:45 -0700

    update sprite code want a sprite cranberry?

--- Teknoloid/AudioVisual/Sprite.cs
+++ Teknoloid/AudioVisual/Sprite.cs
@@ -16,17 +16,9 @@ namespace Teknoloid.AudioVisual
         /// </summary>
         public PositionAnchor Anchor { get; set; }
         /// <summary>
-        /// The X position of the sprite.
+        /// The position of the sprite.
         /// </summary>
-        public float X { get; set; }
-        /// <summary>
-        /// The Y position of the sprite.
-        /// </summary>
-        public float Y { get; set; }
-        /// <summary>
-        /// The draw layer of the sprite. Higher value = closer to the front of the screen
-        /// </summary>
-        public int Z { get; set; }
+        public Vector3 Position { get; set; }
         /// <summary>
         /// idk lmfao who do you think i am, the color witch?
         /// </summary>
@@ -38,78 +30,18 @@ namespace Teknoloid.AudioVisual
         /// <summary>
         /// The size of the image.
         /// </summary>
-        public float Scale { get; set; }
+        public Vector2 Scale { get; set; }
 
-        
-        public Sprite(string path, float x, float y)
+        public Sprite(string path, Vector3 pos, PositionAnchor anchor, float opacity, Vector2 scale, Color aColor)
         {
-            // Set parameters
+            // Set parameter values
             SpritePath = path;
-            X = x;
-            Y = y;
-
-            // Set default values
-            Anchor = PositionAnchor.Center;
-            Z = 0;
-            AdditiveColor = Color.White;
-            Opacity = 1.0f;
-            Scale = 1.0f;
-        }
-        public Sprite(string path, float x, float y, int z)
-        {
-            // Set parameters
-            SpritePath = path;
-            X = x;
-            Y = y;
-            Z = z;
-
-            // Set default values
-            Anchor = PositionAnchor.Center;
-            AdditiveColor = Color.White;
-            Opacity = 1.0f;
-            Scale = 1.0f;
-        }
-        public Sprite(string path, float x, float y, PositionAnchor anchor)
-        {
-            // Set parameters
-            SpritePath = path;
-            X = x;
-            Y = y;
-            Anchor = anchor;
-
-            // Set default values
-            Z = 0;
-            AdditiveColor = Color.White;
-            Opacity = 1.0f;
-            Scale = 1.0f;
-        }
-        public Sprite(string path, float x, float y, int z, PositionAnchor anchor)
-        {
-            // Set parameters
-            SpritePath = path;
-            X = x;
-            Y = y;
-            Z = z;
-            Anchor = anchor;
-
-            // Set default values
-            AdditiveColor = Color.White;
-            Opacity = 1.0f;
-            Scale = 1.0f;
-        }
-        public Sprite(string path, float x, float y, int z, PositionAnchor anchor, float opacity, float scale, Color acolor)
-        {
-            // Set parameters
-            SpritePath = path;
-            X = x;
-            Y = y;
-            Z = z;
+            Position = pos;
             Anchor = anchor;
             Opacity = opacity;
             Scale = scale;
-            AdditiveColor = acolor;
+            AdditiveColor = aColor;
         }
-
 
     }
 }
